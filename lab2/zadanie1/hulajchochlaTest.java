3d61f64aff5b81aceb8464b759a3b2809ac7ac1b
dgrzesiak <dgrzesiak@sigma.ug.edu.pl>
2015-03-09 16:34:53 +0100

    Update hulajchochlaTest.java

--- lab2/zadanie1/hulajchochlaTest.java
+++ lab2/zadanie1/hulajchochlaTest.java
@@ -15,8 +15,37 @@ public class hulajchochlaTest {
 	@Test
 	public void hulajTest() throws NieduanyPsikusException {
 		int expected = hulaj.hultajchochla(37);
-		int wynik =73;
+		int wynik = 73;
 		assertThat(wynik,is(expected));
+	}
+	@Test
+	public void hulajTest3() throws NieduanyPsikusException {
+		int expected = hulaj.hultajchochla(123);
+		//123 132 213 231 312 321
+		if(expected==132){
+			int wynik = 132;
+		assertThat(wynik,is(expected));
+		}else
+			if(expected == 213)
+			{
+				int wynik = 213;
+				assertThat(wynik,is(expected));
+			}
+			else if(expected == 231)
+			{
+				int wynik = 231;
+				assertThat(wynik,is(expected));
+			} else if (expected == 312)
+			{
+				int wynik = 312;
+				assertThat(wynik,is(expected));
+			}
+			else if (expected == 321)
+			{
+				int wynik = 321;
+				assertThat(wynik,is(expected));
+			} else
+				fail("Nie trafiłeś");
 	}
 	
 }

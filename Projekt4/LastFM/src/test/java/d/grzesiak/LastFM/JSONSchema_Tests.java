630c2e5454fbec964cc185083b121caab2f2ae9e
Michal <mbowzyk@gmail.com>
2015-05-19 11:34:45 +0200

    fixing

--- Projekt4/LastFM/src/test/java/d/grzesiak/LastFM/JSONSchema_Tests.java
+++ Projekt4/LastFM/src/test/java/d/grzesiak/LastFM/JSONSchema_Tests.java
@@ -1,7 +1,6 @@
 package d.grzesiak.LastFM;
 
 import static com.jayway.restassured.RestAssured.*;
-import static org.hamcrest.Matchers.*;
 import static com.jayway.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 import org.junit.BeforeClass;
@@ -13,32 +12,30 @@ import com.jayway.restassured.specification.RequestSpecification;
 
 public class JSONSchema_Tests{
 
-	public static String user1;
-	public static String user2;
+	public static String artist;
+	public static String album;
 	public static String apiKey;
 	public static String format;
 	public static RequestSpecification spec;
-	public static RequestSpecification specLimit;
-	public static ExtractingData data;
+
 	
 	@BeforeClass
 	public static void SetUp(){
 		RestAssured.baseURI = "http://ws.audioscrobbler.com/2.0/";
-		user1 = "potworzlybardzo";
-		user2 = "patwor123";
+		artist = "Pantera";
+		album = "Cowboys from Hell";
 		apiKey = "e4a31927b63bd9c16d7273544ac609bf";
 		format = "json";
 		
 		spec = new RequestSpecBuilder().addParam("api_key", apiKey).addParam("format", format).build();
-		specLimit = new RequestSpecBuilder().addParam("limit", 1).addParam("api_key", apiKey).addParam("format", format).build();
-		data = new ExtractingData(spec, specLimit);
 	}
 	
 	@Test
 	public void topArtistsSchemaValidation(){
-		given().param("user", user1)
-			.spec(specLimit)
+		given().params("artist", artist, "album", album)
+			.spec(spec)
 		.when().get("?method=album.getinfo")
-		.then().body(matchesJsonSchemaInClasspath("albumGetInfoSchema.json"));
+		.then().body(matchesJsonSchemaInClasspath("albumGetInfoSchema.json"))
+			.statusCode(200);
 	}
 }
--- Projekt4/LastFM/src/test/java/d/grzesiak/LastFM/LastFM_Tests.java
+++ Projekt4/LastFM/src/test/java/d/grzesiak/LastFM/LastFM_Tests.java
@@ -2,7 +2,6 @@ package d.grzesiak.LastFM;
 
 import static com.jayway.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
-import static org.hamcrest.MatcherAssert.*;
 
 import org.junit.BeforeClass;
 import org.junit.Test;
@@ -14,80 +13,86 @@ import com.jayway.restassured.specification.RequestSpecification;
 
 public class LastFM_Tests{
 
-	public static String user1;
+	public static String user;
 	public static String user2;
 	public static String apiKey;
 	public static String format;
 	public static RequestSpecification spec;
 	public static RequestSpecification specLimit;
-	public static ExtractingData data;
 	
 	@BeforeClass
 	public static void SetUp(){
 		RestAssured.baseURI = "http://ws.audioscrobbler.com/2.0/";
-		user1 = "potworzlybardzo";
-		user2 = "patwor123";
+		user = "potworzlybardzo";
+		user2 = "angels66";
 		apiKey = "e4a31927b63bd9c16d7273544ac609bf";
 		format = "json";
 		
 		spec = new RequestSpecBuilder().addParam("api_key", apiKey).addParam("format", format).build();
 		specLimit = new RequestSpecBuilder().addParam("limit", 1).addParam("api_key", apiKey).addParam("format", format).build();
-		data = new ExtractingData(spec, specLimit);
 	}
+	
 	@Test
 	public void getTrackOfFavoriteArtist(){
-		String artist = data.getFavoriteArtist(user1);
+		String artist = 
+				given().params("user", user)
+						.spec(specLimit)
+				.when().get("?method=user.gettopartists")
+						.getBody().path("topartists.artist.name").toString();
 		
 		given().params("artist", artist)
-			.spec(specLimit)
+			.spec(spec)
 		.when().get("?method=artist.gettoptracks")
-		.then().body("toptracks.track.name", equalTo("Burn"));
+		.then().body("toptracks.track[1].name", equalTo("War in Heaven"));
 	}
+	
 	@Test
 	public void getTrackOfFavoriteArtistFromTwoUsers(){
-		String artist = data.getFirstArtistFromComparison(user1, user2);
+		String artist = 
+				given().params("type1", "user", "type2", "user",
+						"value1", user, "value2", user2)
+						.spec(specLimit)
+				.when().get("?method=tasteometer.compare")
+						.getBody().path("comparison.result.artists.artist.name").toString();
 		
 		given().params("artist", artist)
 			.spec(specLimit)
 		.when().get("?method=artist.gettoptracks")
-		.then().body("toptracks.track.name", equalTo("Blow Your Trumpets Gabriel"));
+		.then().body("toptracks.track.name", equalTo("Welcome Home"));
 	}
+	
 	@Test
 	public void artistFromComparison(){
 		given().params("type1", "user", "type2", "user",
-				"value1", user1, "value2", user2)
-			.spec(specLimit)
-		.when().get("?method=tasteometer.compare")
-		.then().body("comparison.result.artists.artist.name", equalTo("Behemoth"));
-	}
-	@Test
-	public void artistBySearchingTrack(){
-		given().params("track", "Domination")
+				"value1", user, "value2", user2)
 			.spec(spec)
-		.when().get("?method=track.search")
-		.then().body("results.trackmatches.track.artist", hasItem("Pantera"));
+		.when().get("?method=tasteometer.compare")
+		.then().body("comparison.result.artists.artist[2].name", equalTo("Pantera"));
 	}
+	
+	@Test
+	public void artistSearch(){
+		given().params("artist", "Marley")
+			.spec(specLimit)
+		.when().get("?method=artist.search")
+		.then().body("results.artistmatches.artist.name", equalTo("Bob Marley"));
+	}
+	
 	@Test
 	public void mostPopularArtistAlbum(){
-		given().params("artist", "kat")
-			.spec(specLimit)
+		given().params("artist", "Pink Floyd")
+			.spec(spec)
 		.when().get("?method=artist.gettopalbums")
-		.then().body("topalbums.album.name", equalTo("Ballady"))
-			.statusCode(200);
+		.then().body("topalbums.album.name", hasItem("The Dark Side of the Moon"));
 	}
+	
 	@Test
-	public void mostPopularArtistTrack(){
-		given().params("artist", "iwrestledabearonce")
-			.spec(specLimit)
-		.when().get("?method=artist.gettoptracks")
-		.then().body("toptracks.track.name", equalTo("Tastes Like Kevin Bacon"));
+	public void mostFavoriteUsersAlbums(){
+		given().params("user", user)
+			.spec(spec)
+		.when().get("?method=user.gettopalbums")
+		.then().body("topalbums.album.artist.name", hasItem("Creed"))
+			.body("topalbums.album.artist[1].name", equalTo("The Black Dahlia Murder"));
 	}
-	@Test
-	public void mostFavoriteUsersArtist(){
-		given().params("user", user1)
-			.spec(specLimit)
-		.when().get("?method=user.gettopartists")
-		.then().body("topartists.artist.name", equalTo("Septicflesh"))
-			.statusCode(200);
-	}
+
 }
